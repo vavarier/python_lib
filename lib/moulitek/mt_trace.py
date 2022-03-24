728d6382d8a6c647b0481eb9af82a6417df620d7
vavarier <valentin.nouri20@gmail.com>
2022-03-24 02:26:36 +0100

    push

--- lib/moulitek/mt_trace.py
+++ lib/moulitek/mt_trace.py
@@ -5,40 +5,40 @@ moulitek_script_trace = ""
 
 def success(name):
     global moulitek_script_trace
-    moulitek_script_trace += "[OK] " + name + " [/OK]"
-    moulitek_script_trace += "_____________________________________\n"
+    moulitek_script_trace += "[OK] " + name + " [/OK]\n"
+    moulitek_script_trace += "_____________________________________\n\n"
 
 def category(name, description):
     global moulitek_script_trace
-    moulitek_script_trace += "[##] " + name + " [/##]\n"
+    moulitek_script_trace += "[##] " + name + " [/##]\n\n"
     if (description != None):
-        moulitek_script_trace += "[desc]\n" + description + "\n[/desc]"
-    moulitek_script_trace += "_____________________________________\n"
+        moulitek_script_trace += "[desc]\n" + description + "\n[/desc]\n"
+    moulitek_script_trace += "_____________________________________\n\n"
 
 def sequence(name, description):
     global moulitek_script_trace
-    moulitek_script_trace += "[#] " + name + " [/#]\n"
+    moulitek_script_trace += "[#] " + name + " [/#]\n\n"
     if (description != None):
-        moulitek_script_trace += "[desc]\n" + description + "\n[/desc]"
-    moulitek_script_trace += "_____________________________________\n"
+        moulitek_script_trace += "[desc]\n" + description + "\n[/desc]\n"
+    moulitek_script_trace += "_____________________________________\n\n"
 
 def error(name, flag, description, expected, got):
     global moulitek_script_trace
     if (flag == SEGFAULT):
-        moulitek_script_trace += "[KO] [segfault] " + name + " [/KO]\n"
+        moulitek_script_trace += "[KO] [segfault] " + name + " [/KO]\n\n"
     if (flag == TIMEOUT):
-        moulitek_script_trace += "[KO] [timeout] " + name + " [/KO]\n"
+        moulitek_script_trace += "[KO] [timeout] " + name + " [/KO]\n\n"
     if (flag == NEVER_RUN):
         return ;
     if (flag == RETVALUE):
-        moulitek_script_trace += "[KO] [retvalue] " + name + " [/KO]\n"
+        moulitek_script_trace += "[KO] [retvalue] " + name + " [/KO]\n\n"
     if (flag == BADOUTPUT):
-        moulitek_script_trace += "[KO] [badoutput] " + name + " [/KO]\n"
+        moulitek_script_trace += "[KO] [badoutput] " + name + " [/KO]\n\n"
     if (description != None):
-        moulitek_script_trace += "[desc]\n" + description + "\n[/desc]\n"
-    moulitek_script_trace += "[expected]\n" + expected + "\n[/expected]\n"
-    moulitek_script_trace += "[got]\n" + got + "\n[/got]\n"
-    moulitek_script_trace += "_____________________________________\n"
+        moulitek_script_trace += "[desc]\n" + description + "\n[/desc]\n\n"
+    moulitek_script_trace += "[expected]\n" + expected + "\n[/expected]\n\n"
+    moulitek_script_trace += "[got]\n" + got + "\n[/got]\n\n"
+    moulitek_script_trace += "_____________________________________\n\n"
 
 
 def gen_trace():
