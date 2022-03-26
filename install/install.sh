c30b039251c2f5688c7d0d6b40f95b35e9eadc25
Mathias A <mathias.andre@epitech.eu>
2022-03-26 19:35:54 +0100

    [FIX] Fixed execution redirect.

--- install/install.sh
+++ install/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "=> Browse to find old versions..."
-FILE=/usr/lib/python3.10/site-packages/moulitek
+FILE=/usr/lib/python3/site-packages/moulitek
 if [ -d $FILE ]
 then
     echo "=> Erasing old repository..."
@@ -23,7 +23,7 @@ echo "=> Creating repository..."
 tput sgr0
 
 git clone git@github.com:vavarier/python_lib.git moulitek_lib
-sudo mv moulitek_lib/lib/moulitek /usr/lib/python3.10/site-packages
+sudo mv moulitek_lib/lib/moulitek /usr/lib/python3/site-packages
 rm -rf moulitek_lib
 
 tput setaf 2
--- lib/moulitek/mt_exec.py
+++ lib/moulitek/mt_exec.py
@@ -9,4 +9,4 @@ def call_system(command: str, timeout: int = 60) -> int:
 
     Returns the exit status of the command, or 124 on timeout.
     """
-    return subprocess.call("timeout %ds %s 1> /dev/null 2> /dev/null" % (timeout, command), shell=True)
+    return subprocess.call("timeout %ds %s %s" % (timeout, command, "1> /dev/null 2> /dev/null" if not ">" in command else ""), shell=True)
